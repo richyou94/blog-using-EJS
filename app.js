88df5eb7ee1350ed50634900d1bf0ef2dffa0435
Richard You <richardyou94@gmail.com>
2023-02-11 21:04:54 -0800

    able to post the data into the array, and redirect to home route

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
+const postData = [];
+
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
@@ -38,9 +40,15 @@ app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
-app.post('/compose', function(req, res) {
-  console.log(req.body.postTitle);
-})
+app.post("/compose", function (req, res) {
+  const post = {
+    title: req.body.postTitle,
+    description: req.body.postDescr,
+  };
+postData.push(post);
+console.log(postData);
+res.redirect('/');
+});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
