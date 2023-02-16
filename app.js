faecd38c10c766e1f0a986e6e29752db1960f9eb
Richard You <richardyou94@gmail.com>
2023-02-15 23:55:11 -0800

    compose page, post request work adding data

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
 const _ = require("lodash");
-const postData = [];
+const mongoose = require("mongoose");
+
+// const postData = [];
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -19,6 +20,28 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+  useNewUrlParser: true,
+});
+
+const blogsSchema = {
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+};
+
+const Blog = mongoose.model('Blog', blogsSchema);
+
+const blog = new Blog({
+  title: "Welcome to the blog",
+  description: "This is sample initial data of the blog."
+})
 
 app.get("/", function (req, res) {
   res.render("home", {
@@ -44,30 +67,31 @@ app.get("/compose", function (req, res) {
 app.get("/posts/:postTitle", (req, res) => {
   const requestTitle = _.lowerCase(req.params.postTitle);
 
-  postData.forEach(post => {
+  postData.forEach((post) => {
     const storedTitle = _.lowerCase(post.title);
 
     if (storedTitle === requestTitle) {
-      console.log('match found');
+      console.log("match found");
       res.render("post", {
         postTitle: post.title,
         postDescr: post.description,
       });
     } else {
-      console.log('not a match')
+      console.log("not a match");
     }
   });
-
-  
 });
 
 app.post("/compose", function (req, res) {
-  const post = {
+  
+  const blog = new Blog({
     title: req.body.postTitle,
     description: req.body.postDescr,
-  };
-  postData.push(post);
-  res.redirect("/");
+  })
+  
+  blog.save();
+
+  res.redirect("/compose");
 });
 
 app.listen(3000, function () {
